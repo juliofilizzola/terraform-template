0ddba9679564092906b5088ada6ed29cab5535a7
Julio Filizzola <juliofilizzola@hotmail.com>
2026-01-29 13:56:46 -0300

    Add `dev` environment Terraform configuration module

---
+++ environments/dev/main.tf
@@ -0,0 +1,9 @@
+module "dev" {
+  source = "../../infra"
+  app_image_tag = var.app_image_tag
+  cluster_name  = var.cluster_name
+  environment   = var.environment
+  namespace     = var.namespace
+  route53_zone_name = var.route53_zone_name
+  aws_region    = var.aws_region
+}
