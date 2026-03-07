89754e02c3e6d5670b53890b0b126323048055f4
Julio Filizzola <juliofilizzola@hotmail.com>
2026-03-07 15:55:12 -0300

    feat: add VPC, subnets, and SSH key variables to prod environment
    - include `vpc_id`, `private_subnets`, `public_subnets`, and `ssh_key_name` variables in `main.tf` to align with infrastructure requirements

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -6,4 +6,8 @@ module "prod" {
   namespace     = var.namespace
   route53_zone_name = var.route53_zone_name
   aws_region    = var.aws_region
+  public_subnets = var.public_subnets
+  private_subnets = var.private_subnets
+  vpc_id = var.vpc_id
+  ssh_key_name = var.ssh_key_name
 }
