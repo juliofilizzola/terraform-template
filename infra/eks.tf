d750a18ddfd3efa26eb38019dd30cbe08954cbd6
Julio Filizzola <juliofilizzola@hotmail.com>
2026-03-07 15:54:04 -0300

    feat: upgrade EKS module version and enhance cluster configuration
    details:
    - bump EKS module to version 20.24.1 for improvements
    - make Kubernetes version configurable via `var.kubernetes_version`
    - enable fine-grained control over cluster endpoint access settings
    - update managed node group with new parameters (disk size, AMI type)
    - add support for cluster addons (CoreDNS, kube-proxy, VPC CNI)
    - include environment and cluster type tagging

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,25 +1,48 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "~> 20.24.1"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.33"
+  cluster_version = var.kubernetes_version
+
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
 
-  public_subnet_ids = var.public_subnets
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_private_access_cidrs = var.cluster_endpoint_private_access_cidrs
 
   eks_managed_node_groups = {
-    general = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+    workers = {
+      name           = "bpcode-workers"
+      use_name_prefix = false
 
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
-      key_name       = var.ssh_key_name
+
+      min_size     = 3
+      max_size     = 3
+      desired_size = 3
+
+      ami_type = "AL2_x86_64_STANDARD"
+      disk_size = 20
+
+      vpc_security_group_ids = [aws_security_group.eks_workers.id]
     }
   }
 
+  cluster_addons = {
+    coredns    = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni    = { most_recent = true }
+  }
+
   enable_irsa = true
+
+  tags = {
+    Environment = var.environment
+    ClusterType = "pegasus"
+  }
 }
